afe4bb6f36b5b2bccfb2fac5f425eb5d244fb5e5
youngphil <phil@petcity.io>
2018-01-26 16:27:21 +0900

    fixed Tile

--- src/components/ZhedTile.js
+++ src/components/ZhedTile.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import classnames from 'classnames';
+import ZhedButton from './ZhedButton';
+import ZhedDot from './ZhedDot';
 import './ZhedTile.css';
 
 class ZhedTile extends React.Component {
@@ -10,10 +13,26 @@ class ZhedTile extends React.Component {
       indicator: props.indicator,
     };
   }
+  renderButton(block) {
+    if (block !== '0') {
+      return <ZhedButton block={block} onClick={this.props.onClick} />;
+    }
+    return null;
+  }
+  renderDot(dot) {
+    if (dot) {
+      return <ZhedDot onClick={this.props.onClick} />;
+    }
+    return null;
+  }
   render() {
+    const color = this.state.background;
+    const block = this.state.zhedBlock;
+    const dot = this.state.indicator;
     return (
-      <div className="Zhed-tile">
-        {this.state.zhedBlock}
+      <div className={classnames('Zhed-tile', 'p-1', `gray-${color}00`)}>
+        {this.renderButton(block)}
+        {this.renderDot(dot)}
       </div>
     )
   }

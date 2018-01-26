7f1980834877a6fda7f06f71c83412b45b4961d7
youngphil <phil@petcity.io>
2018-01-26 16:26:56 +0900

    fixed util

--- src/util/index.js
+++ src/util/index.js
@@ -3,3 +3,9 @@ export function randomMap(arr) {
     return row.map(col => Math.floor(Math.random() * (4)) + 1);
   });
 }
+
+export function nullMap(arr) {
+  return arr.map(row => {
+    return row.map(col => false);
+  });
+}

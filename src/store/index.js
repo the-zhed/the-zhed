27c376b90c30ea4d801d9305cd2004a9e522a8bd
GimYoungPhil <youngphil.gim@gmail.com>
2018-01-28 03:08:31 +0900

    fixed redux_devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 import zhedApp from '../reducers';
 
-const store = createStore(zhedApp);
+const store = createStore(
+  zhedApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;

bb3b1282a10f1e39a12cfe671284ebf4db6207db
GimYoungPhil <youngphil.gim@gmail.com>
2018-02-01 23:33:32 +0900

    fixed PackList

--- src/components/pack/PackList.js
+++ src/components/pack/PackList.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const PackList = ({ list, history }) => (
+const PackList = ({ list }) => (
   <div className="container">
     <div className="row">
       {list.map((item) => (
         <div className="col-3" key={item.level}>
-          <button
-            onClick={() => history.push(`/zhed/${item.level}`)}
-            className="btn btn-dark"
-            disabled={!item.enabled}
-          >
-            {item.level}
-          </button>
+          {item.enabled ? (
+            <Link to={`/zhed/${item.level}`} className="btn btn-dark">
+              {item.level}
+            </Link>
+          ) : (
+            <button className="btn btn-dark" disabled>{item.level}</button>
+          )}
         </div>
       ))}
     </div>
   </div>
 )
 
-export default withRouter(PackList);
+export default PackList;

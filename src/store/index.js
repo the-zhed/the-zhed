5e679e1e38c9dd6c7585cad6d85be0939cbdbd46
youngphil <phil@petcity.io>
2018-02-02 13:29:07 +0900

    add redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import logger from 'redux-logger'
 import zhedApp from '../reducers';
 
 const store = createStore(
   zhedApp,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunkMiddleware),
+  applyMiddleware(
+    thunkMiddleware,
+    logger,
+  ),
 );
 
 export default store;

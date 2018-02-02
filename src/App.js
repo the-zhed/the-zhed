007e2d38fe119c9c9a090cd0cf5a927afb12fcb5
youngphil <phil@petcity.io>
2018-02-02 17:58:15 +0900

    fixed Root -> App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React, { Component } from 'react';
-import ZhedBoard from './components/ZhedBoard';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
-import Level from './components/Level';
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Pack from './components/Pack';
+import Zhed from './components/Zhed';
+import store from './store';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to Zhed</h1>
-        </header>
-        <div className="container">
-          <ZhedBoard />
-        </div>
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/pack">Pack</Link></li>
+        </ul>
+        <hr/>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/pack/" component={Pack}/>
+        <Route path="/zhed/:level" component={Zhed}/>
       </div>
-    );
-  }
-}
+    </Router>
+  </Provider>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Root from './Root';
+import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Root />,
+  <App />,
   document.getElementById('root')
 );
 // registerServiceWorker();

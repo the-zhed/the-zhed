461176fa5aba917acad800d3b79bcc2600fb9a13
GimYoungPhil <youngphil.gim@gmail.com>
2018-02-15 03:57:41 +0900

    add ActionTypes

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,27 @@
+// currentPack
+export const SELECT_CURRENT_PACK     = 'currentPack/SELECT_CURRENT_PACK';
+// packList
+export const INITIALIZE_PACK_LIST    = 'packList/INITIALIZE_PACK_LIST';
+export const ENABLED_LEVEL_PACK      = 'packList/ENABLED_LEVEL_PACK';
+// currentLevel
+export const SELECT_CURRENT_LEVEL    = 'currentLevel/SELECT_CURRENT_LEVEL';
+// blockMapList
+export const REQUEST_BLOCK           = 'blockMapList/REQUEST_BLOCK';
+export const RECEIVE_BLOCK           = 'blockMapList/RECEIVE_BLOCK';
+// backgroundMap
+export const INITIALIZE_BG_MAP       = 'backgroundMap/INITIALIZE_BG_MAP';
+// blockMap
+export const INITIALIZE_BLOCK_MAP    = 'blockMap/INITIALIZE_BLOCK_MAP';
+export const UNFOLD_BLOCK_MAP        = 'blockMap/UNFOLD_BLOCK_MAP';
+export const RESTART_BLOCK_MAP       = 'blockMap/RESTART_BLOCK_MAP';
+export const UNDO_BLOCK_MAP          = 'blockMap/UNDO_BLOCK_MAP';
+// indicatorMap
+export const RESET_INDICATOR_MAP     = 'indicatorMap/RESET_INDICATOR_MAP';
+export const UNFOLD_INDICATOR_MAP    = 'indicatorMap/UNFOLD_INDICATOR_MAP';
+// indicatorList
+export const RESET_INDICATOR_LIST    = 'indicatorList/RESET_INDICATOR_LIST';
+export const STACK_INDICATOR_LIST    = 'indicatorList/STACK_INDICATOR_LIST';
+// blockMapHistory
+export const RESET_BLOCK_MAP_HISTORY = 'blockMapHistory/RESET_BLOCK_MAP_HISTORY';
+export const NEXT_BLOCK_MAP_HISTORY  = 'blockMapHistory/NEXT_BLOCK_MAP_HISTORY';
+export const UNDO_BLOCK_MAP_HISTORY  = 'blockMapHistory/UNDO_BLOCK_MAP_HISTORY';

d9c1597e3ef6f5c579340300dfd542977acc7dfc
GimYoungPhil <youngphil.gim@gmail.com>
2018-02-17 23:21:44 +0900

    fixed store

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-import { apiMiddleware } from 'redux-api-middleware';
-import zhedApp from '../reducers';
+import root from '../reducers/root';
 
 const store = createStore(
-  zhedApp,
+  root,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(
-    logger,
     thunkMiddleware,
-    apiMiddleware,
+    logger,
   ),
 );
 

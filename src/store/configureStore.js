a097368262337093ff524633926acb8a699712bf
GimYoungPhil <youngphil.gim@gmail.com>
2018-03-03 01:17:46 +0900

    fixed middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
 import _ from 'underscore';
 import root from '../reducers/root';
 import { loadState, saveState } from '../util/localStorage';
@@ -9,9 +8,6 @@ import { loadState, saveState } from '../util/localStorage';
 const configureStore = () => {
   const persistedState = loadState();
   const middleware = [thunkMiddleware];
-  if (process.env.NODE_ENV !== 'production') {
-    middleware.push(createLogger());
-  }
   const store = createStore(
     root,
     persistedState,
